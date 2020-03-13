5795353140ae800259b8a80744797f62fedec944
root <s18924@pjwstk.edu.pl>
2020-03-13 16:39:38 -0500

    yoo

--- Zad1/Zad1/Program.cs
+++ Zad1/Zad1/Program.cs
@@ -1,12 +1,81 @@
-﻿using System;
+﻿﻿using System;
+using System.Collections.Generic;
+using System.IO;
+ using System.Xml;
+ using System.Xml.Serialization;
 
-namespace Zad1
+namespace Cwiczenia_II
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Podaj lokalizację pliku!");
+            string par1 = Console.ReadLine();
+
+            Console.WriteLine("Podaj plik wyjściowy!");
+            string par2 = Console.ReadLine();
+
+            var lokalizacja = (String.IsNullOrEmpty(par1) ? "dane.csv" : par1);
+            var output = (String.IsNullOrEmpty(par2) ? "result.xml" : par2);
+
+            Console.WriteLine(lokalizacja);
+            Console.WriteLine(output);
+
+            var lines = File.ReadAllLines(lokalizacja); //tablica stringow jeden to jedna linijka
+
+            var list = new List<Student>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i].Split(",");
+
+                for (int j = 0; j < line.Length; j++)
+                {
+                    Console.Write(line[j]);
+                    Studia s = new Studia
+                    {
+                        name = line[6],
+                        mode = line[7]
+                    };
+
+                    list.Add(new Student
+                    {
+                        Imie = line[0],
+                        Nazwisko = line[1],
+                        Data_urodzenia = line[2],
+                        Email = line[3],
+                        Imie_matki = line[4],
+                        Imie_Ojca = line[5],
+                        Studia = s
+                    }) ;
+                    //list.Add(new Student(line[0], line[1], line[2], line[3], line[4], line[5]));
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(par1);
+            FileStream writer = new FileStream(@"data.xml", FileMode.Create);   
+            //?
+           // var attr = new XmlRootAttribute("uczelnia");
+           Studia ss = new Studia()
+           {
+               mode = "1112",
+               name = "wewe"
+           };
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("ssss"));
+
+            serializer.Serialize(writer, list);
         }
+
+
     }
+
+  /*  FileStream writer = new FileStream(@"data.xml", FileMode.Create);
+    XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("uczelnia"));
+    var list = new List<Student>();
+    list.Add(new Student { Imie = "Jan", Nazwisko = "Kowalski" });
+    
+     serializer.Serialize(writer, list);*/
 }
---
+++ Zad1/Zad1/Student.cs
@@ -0,0 +1,37 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cwiczenia_II
+{
+   public class Student
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Data_urodzenia { get; set; }
+        public string Email { get; set; }
+        public string Imie_matki { get; set; }
+        public string Imie_Ojca { get; set; }
+
+        public Studia Studia { get; set; }
+
+
+
+        /* public Student(string imie, string nazwisko, string data_urodzenia, string email, string imie_matki, string imie_Ojca)
+         {
+             Imie = imie;
+             Nazwisko = nazwisko;
+             Data_urodzenia = data_urodzenia;
+             Email = email;
+             Imie_matki = imie_matki;
+             Imie_Ojca = imie_Ojca;
+         }*/
+    }
+
+    public class Studia
+    {
+        public string name  { get; set; }
+        public string mode { get; set; }
+
+    }
+}
