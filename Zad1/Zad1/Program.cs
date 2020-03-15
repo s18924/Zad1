4d88f50234ada70592ad84189bb34ee601cf5f1b
root <s18924@pjwstk.edu.pl>
2020-03-15 16:54:13 -0500

    zzzzzzzz zzzzzzz

--- Zad1/Zad1/Program.cs
+++ Zad1/Zad1/Program.cs
@@ -17,41 +17,41 @@ using System.IO;
             using (StreamWriter fw = new StreamWriter("log.txt"))
             {
 
-           
-            Console.WriteLine("Podaj lokalizację pliku!");
-            string par1 = Console.ReadLine();
 
-            var lokalizacja = (String.IsNullOrEmpty(par1) ? "dane.csv" : par1);
+                Console.WriteLine("Podaj lokalizację pliku!");
+                string par1 = Console.ReadLine();
 
-            if (!File.Exists(lokalizacja))
-            {
-                Console.Error.Write("File not found!");
-                fw.Write("File not found! : " + lokalizacja + " exiting program!");
-                fw.Dispose();
-                Environment.Exit(1);
-            }
+                var lokalizacja = (String.IsNullOrEmpty(par1) ? "dane.csv" : par1);
+
+                if (!File.Exists(lokalizacja))
+                {
+                    Console.Error.Write("File not found!");
+                    fw.Write("File not found! : " + lokalizacja + " exiting program!");
+                    fw.Dispose();
+                    Environment.Exit(1);
+                }
 
                 Console.WriteLine("Podaj plik wyjściowy!");
-            string par2 = Console.ReadLine();
+                string par2 = Console.ReadLine();
 
-            var output = (String.IsNullOrEmpty(par2) ? "result.xml" : par2);
+                var output = (String.IsNullOrEmpty(par2) ? "result.xml" : par2);
 
-            Console.WriteLine(lokalizacja);
-            Console.WriteLine(output);
+                Console.WriteLine(lokalizacja);
+                Console.WriteLine(output);
 
-            var lines = File.ReadAllLines(lokalizacja); //tablica stringow jeden to jedna linijka
+                var lines = File.ReadAllLines(lokalizacja); //tablica stringow jeden to jedna linijka
 
 
-            bool czyZapisac = true;
-            
+                bool czyZapisac = true;
 
-           
+
+
                 for (int i = 0; i < lines.Length; i++)
                 {
                     var line = lines[i].Split(",");
                     czyZapisac = true;
 
-                    
+
                     foreach (var z in line)
                     {
                         if (String.IsNullOrEmpty(z))
@@ -78,68 +78,70 @@ using System.IO;
                     if (czyZapisac)
                     {
                         Studia s = new Studia
-                            {
-                                name = line[2],
-                                mode = line[3]
-                            };
+                        {
+                            name = line[2],
+                            mode = line[3]
+                        };
 
-                            list.Add(new Student
-                            {
-                                Imie = line[0],
-                                Nazwisko = line[1],
-                                Data_urodzenia = line[5],
-                                Email = line[6],
-                                Imie_matki = line[7],
-                                Imie_Ojca = line[8],
-                                eska = line[4],
-                                Studia = s
-                            });
-       
+                        list.Add(new Student
+                        {
+                            Imie = line[0],
+                            Nazwisko = line[1],
+                            Data_urodzenia = line[5],
+                            Email = line[6],
+                            Imie_matki = line[7],
+                            Imie_Ojca = line[8],
+                            eska = line[4],
+                            Studia = s
+                        });
+
                     }
 
                 }
-            }
 
-            Dictionary<string, int> hash = new Dictionary<string, int>();
 
-            for (int i = 0; i < list.Count; i++)
-            {
-                if (hash.ContainsKey(list[i].Studia.name))
-                    hash[list[i].Studia.name]++;
-                
-                if (!hash.ContainsKey(list[i].Studia.name))
-                    hash.Add(list[i].Studia.name,1);
-                
-            }
+                Dictionary<string, int> hash = new Dictionary<string, int>();
 
-            List<Studia> l = new List<Studia>();
-
-            foreach (var i in hash)
-            {
-                Console.WriteLine(i.Key +  " " + i.Value);
-                l.Add(new Studia()
+                for (int i = 0; i < list.Count; i++)
                 {
-                    name2 = i.Key,
-                    numberOfStudents = i.Value
-                });
+                    if (hash.ContainsKey(list[i].Studia.name))
+                        hash[list[i].Studia.name]++;
+
+                    if (!hash.ContainsKey(list[i].Studia.name))
+                        hash.Add(list[i].Studia.name, 1);
+
+                }
+
+                List<Studia> l = new List<Studia>();
+
+                foreach (var i in hash)
+                {
+                    Console.WriteLine(i.Key + " " + i.Value);
+                    l.Add(new Studia()
+                    {
+                        name2 = i.Key,
+                        numberOfStudents = i.Value
+                    });
+                }
+
+
+                FileStream writer = new FileStream(output, FileMode.Create);
+
+                XmlSerializer serializer = new XmlSerializer(typeof(Uczelnia));
+
+                XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+                ns.Add("", "");
+                Uczelnia u = new Uczelnia()
+                {
+                    createdAt = DateTime.Today.ToShortDateString(),
+                    Author = "Szymon Pierzchała",
+                    studenci = list,
+                    activeStudies = l
+
+                };
+                serializer.Serialize(writer, u, ns);
+                writer.Dispose();
             }
-
-
-            FileStream writer = new FileStream(@"data.xml", FileMode.Create);   
-     
-            XmlSerializer serializer = new XmlSerializer(typeof(Uczelnia));
-           // var xns = new XmlSerializerNamespaces();
-          //  xns.Add(string.Empty, string.Empty);
-            Uczelnia u = new Uczelnia()
-            {
-                createdAt = DateTime.Today.ToShortDateString(),
-                Author = "Szymek",
-                studenci = list,
-                activeStudies = l
-                
-            };
-            serializer.Serialize(writer, u);
-            writer.Dispose();
         }
 
 
